44f79052f673bedd13856c3975bbf83db6702d68
gabiprosfriends <127973347+gabiprosfriends@users.noreply.github.com>
2023-03-22 15:58:03 +0000

    No more spaces

---
+++ script.js
@@ -0,0 +1,8 @@
+const numElement = document.getElementById('num');
+const resultElement = document.getElementById('result');
+
+    for(let i = 0; i <= 10; i++)
+    {
+        resultElement = numElement * i;
+        (numElement, i , resultElement);
+    }
