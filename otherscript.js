1352a1b9602258bea18c0a5dc96fb3a2c8b14520
gabiprosfriends <127973347+gabiprosfriends@users.noreply.github.com>
2023-03-23 11:11:29 +0000

    No moore spaces

--- otherscript.js
+++ otherscript.js
@@ -3,22 +3,24 @@ const btn = document.getElementById('go');
 const table = document.getElementById('tabuada');
 
 btn.addEventListener("click", () => {
-    console.log(numElement.value)
+    innerHTML = null;
     for (let i = 0; i <= 10; i++) {
         const line = document.createElement("tr");
         const multiplicador = document.createElement("th");
+        const mult = document.createElement("th")
         const multiplicando = document.createElement("th");
+        const result = document.createElement("th");
         const produto = document.createElement("th");
-        const vezes = ["x"];
-        const result = ["="];
-
+        
+        mult.innerHTML = "x";
+        result.innerHTML = "=";
         multiplicador.innerHTML = numElement.value;
         multiplicando.innerHTML = i;
         produto.innerHTML = numElement.value * i;
         line.appendChild(multiplicador);
-        line.appendChild(vezes());
+        line.appendChild(mult);
         line.appendChild(multiplicando);
-        line.appendChild(result());
+        line.appendChild(result);
         line.appendChild(produto);
         table.appendChild(line);
     }
