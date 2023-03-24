3f98937c61f663649541723f02705c2f767b6742
gabiprosfriends <127973347+gabiprosfriends@users.noreply.github.com>
2023-03-24 00:03:18 +0000

    finally

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-const numElement = document.getElementById('num');
-const resultElement = document.getElementById('result');
+const numero = document.getElementById('dados');
+const botao = document.getElementById('iniciar');
+const tabelatabuada = document.getElementById('tabela');
 
-    for(let i = 0; i <= 10; i++)
-    {
-        resultElement = numElement * i;
-        (numElement, i , resultElement);
+botao.addEventListener("click", () => {
+    tabelatabuada.innerHTML = null;
+    for (let i = 0; i <= 10; i++) {
+        const line = document.createElement("tr");
+        const multiplicador = document.createElement("td");
+        const mult = document.createElement("td")
+        const multiplicando = document.createElement("td");
+        const simboloigual = document.createElement("td");
+        const resultadoconta = document.createElement("td");
+
+        mult.innerHTML = "x";
+        simboloigual.innerHTML = "=";
+        multiplicador.innerHTML = numero.value;
+        multiplicando.innerHTML = i;
+        resultadoconta.innerHTML = numero.value * i;
+        line.appendChild(multiplicador);
+        line.appendChild(mult);
+        line.appendChild(multiplicando);
+        line.appendChild(simboloigual);
+        line.appendChild(resultadoconta);
+        tabelatabuada.appendChild(line);
     }
+});
